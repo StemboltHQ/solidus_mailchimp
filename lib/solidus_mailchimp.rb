27d61c7b81042a13284451d4ea1b73514fd5800a
Gray Gilmore <graygilmore@gmail.com>
2017-09-07 11:02:00 -0700

    bundle gem solidus_mailchimp

---
+++ lib/solidus_mailchimp.rb
@@ -0,0 +1,5 @@
+require "solidus_mailchimp/version"
+
+module SolidusMailchimp
+  # Your code goes here...
+end
---
+++ test/solidus_mailchimp_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class SolidusMailchimpTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::SolidusMailchimp::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
